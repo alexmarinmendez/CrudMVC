3a324667dee55dcec14d5dfe08b8ba9c292d8d0b
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-04-16 18:38:05 -0500

    Agregar archivos de proyecto.

---
+++ CrudMVC/Controllers/InicioController.cs
@@ -0,0 +1,48 @@
+using CrudMVC.Data;
+using CrudMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace CrudMVC.Controllers
+{
+    public class InicioController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InicioController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Contacto.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Contacto.Add(contacto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CrudMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CrudMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudMVC.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        // Agregar los modelos
+        public DbSet<Contacto> Contacto {  get; set; }
+    }
+}
